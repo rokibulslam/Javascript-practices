53bb839c5f62169636cde2559de97c5c076f877e
rokibulslam <rokibulslam6@gmail.com>
2022-12-02 19:45:52 +0600

    basic array related problem

--- basic1.js
+++ basic1.js
@@ -44,3 +44,43 @@ for (let i = 11; i <=30; i+=2) {
     odd += i;
 }
 console.log(odd);
+
+// Coding challenge #8: Create a function that will convert from Celsius to Fahrenheit
+
+function celciousToFarenheit(n) {
+    return n * 1.8 + 32;
+}
+const t = celciousToFarenheit(5)
+console.log(t)
+
+// Coding challenge #9: Create a function that will convert from Fahrenheit to Celsius
+
+function farenheitToCelcious(n) {
+    return (n - 32) / 1.8;
+}
+const c = farenheitToCelcious(60);
+console.log(c.toFixed(2));
+
+// Coding challenge #10: Calculate the sum of numbers in an array of numbers
+function sumArray(ar) {
+    let sum = 0;
+    for (let i = 0; i < ar.length; i++){
+        sum =sum + ar[i];
+    }
+    console.log(sum)
+    return sum;
+}
+const sumOfArray = sumArray([2, 3, 4, 5])
+console.log(sumOfArray);
+
+// Coding challenge #11: Calculate the average of the numbers in an array of numbers
+function average(array) {
+    const length = array.length;
+    let sum = 0;
+    for (let i = 0; i < array.length; i++){
+        sum += array[i]
+    }
+    return sum / length;
+}
+const av = average([2, 3, 4, 5, 6])
+console.log("average", av);
